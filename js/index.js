fe98958198e0145cedd76a6c09670fe3931b7406
Shitesh Arihunta <arihunta@gmail.com>
2025-05-25 21:08:42 +0200

    Fix zoom level & background colour

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([-34.00, 18.45], 12);
+var map = L.map('map').setView([-34.00, 18.45], 11);
 
-L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm-lite/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
